481f7f581c21a8dd83378ecf17b6852664b40329
Tamas Levai <levait@tmit.bme.hu>
2016-10-30 11:42:38 +0100

    Implement web to server solo notification

--- src/web/hans_web.py
+++ src/web/hans_web.py
@@ -1,8 +1,19 @@
+import socket
+from flask import Flask, render_template, redirect, url_for, request
 
-from flask import Flask, render_template, redirect, url_for, request, jsonify
-
+hans_addr = 'localhost'
+hans_port = 9999
 app = Flask(__name__)
-#app.config['SERVER_NAME'] = 'hansmusic:5000'
+
+
+def clickCount():
+    clickCount.clicks += 1
+    if(clickCount.clicks == 5):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto('solo', (hans_addr, hans_port))
+        clickCount.clicks = 0
+clickCount.clicks = 0
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -12,20 +23,14 @@ def index():
     client = request.remote_addr
     return render_template('index.html', ip=client, reply=msg)
 
+
 @app.route('/hanssolo', methods=['POST'])
 def hanssolo():
-    #hs = request.form['hanssolo']
+    # hs = request.form['hanssolo']
     clickCount()
     msg = "Wait for the magic!"
-    return redirect(url_for('index', reply = msg))
+    return redirect(url_for('index', reply=msg))
 
-def clickCount():
-    clickCount.clicks += 1
-    if(clickCount.clicks == 5):        
-        print(5) # ITT MEHET KI A SOCKET
-        clickCount.clicks = 0
-clickCount.clicks = 0
 
 if __name__ == '__main__':
-    #app.config['SERVER_NAME'] = 'hansmusic:5000'
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
