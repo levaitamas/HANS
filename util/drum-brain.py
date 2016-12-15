88305f5315c490f6f26041fd3ef5cf929933ee61
Tamas Levai <levait@tmit.bme.hu>
2016-12-15 23:11:51 +0100

    Fix several drum brain issues
    Fix copyright boilerplate.
    Add min value to reverb's damp parameter.
    Remove trailing whitespaces.
    Set default sampleroot to '.'.

--- util/drum-brain.py
+++ util/drum-brain.py
@@ -3,9 +3,15 @@
 """
 HANS DRUM UTIL
 
-Copyright (C) 2016-     Rich�rd Beregi <richard.beregi@sztaki.mta.hu>
-Copyright (C) 2016-     Tam�s L�vai    <levait@tmit.bme.hu>
+Copyright (C) 2016-     Richárd Beregi <richard.beregi@sztaki.mta.hu>
+Copyright (C) 2016-     Tamás Lévai    <levait@tmit.bme.hu>
 """
+try:
+    import wx
+    if str.startswith(wx.version(), '2'):
+        wx.SL_VALUE_LABEL = 0
+except ImportError:
+    raise SystemError("wxPython not found. Please, install it.")
 try:
     from pyo import *
 except ImportError:
@@ -15,7 +21,6 @@ import fnmatch
 import os
 import sys
 import random
-import wx
 import time
 
 
@@ -24,13 +29,13 @@ class Sample:
         assert path
         self.path = path
         self.category = category or os.path.basename(os.path.dirname(path))
-        self.audio = SndTable(path)
+        self.audio = SndTable(path, chnl=1)
         self.audio_rate = self.audio.getRate()
 
 
 class Chooser:
     def __init__(self, modulator, sample_root='.'):
-        self.modulator = modulator        
+        self.modulator = modulator
         self.sample_root = sample_root
         self.samples =  { 'kickL': None, 'kickM': None, 'kickH': None,
                           'snareL': None, 'snareM': None, 'snareH': None,
@@ -56,9 +61,10 @@ class Chooser:
 
     def load_samples(self, folder):
         for root, dirnames, filenames in os.walk(folder):
-            for filename in fnmatch.filter(filenames, '*.wav'):
+            for filename in fnmatch.filter(filenames, '*.aiff'):
                 path = os.path.join(root, filename)
-                self.samples[os.path.basename(filename)] = Sample(path)
+                self.samples[
+                    os.path.basename(filename).split('.')[0]] = Sample(path)
 
 
 class MidiProc:
@@ -92,10 +98,10 @@ def handle_midievent(status, note, velocity):
             midinote2sample('crash', velocity, low, high)
         # filter ride
         elif note == 51:
-            midinote2sample('ride', velocity, low, high) 
+            midinote2sample('ride', velocity, low, high)
         # filter hh open
         elif note == 46:
-            midinote2sample('hhO', velocity, low, high) 
+            midinote2sample('hhO', velocity, low, high)
         # filter hh close
         elif note == 42:
             midinote2sample('hhC', velocity, low, high)
@@ -106,13 +112,13 @@ def handle_midievent(status, note, velocity):
     elif 192 <= status <= 207:
         chooser.set_kit('DK%s' % note)
     #filter control change
-    #elif 176 <= status <= 191 and note == 4:        
+    #elif 176 <= status <= 191 and note == 4:
 
 def midinote2sample(pad, velocity, low, high):
     if velocity <= low:
         chooser.execute('%sL' % pad)
     elif velocity >= high:
-        chooser.execute('%sH' % pad)            
+        chooser.execute('%sH' % pad)
     else:
         chooser.execute('%sM' % pad)
 
@@ -168,18 +174,18 @@ class Modulator:
         if self.effectchain['Reverb']:
             self.output = Freeverb(chorus,
                                    size=self.effectchain['Reverb-param'],
-                                   damp=random.random(), 
+                                   damp=0.6 + random.random(),
                                    bal=0.7)
         else:
             self.output = chorus
         self.output.out()
 
     def toggle_effect(self, name, state):
-        if name in self.effectchain:          
+        if name in self.effectchain:
             self.effectchain[name] = state
 
     def set_effect_param(self, param, value):
-        if param in self.effectchain:          
+        if param in self.effectchain:
             self.effectchain[param] = value
 
 
@@ -201,7 +207,7 @@ class HansDrumFrame(wx.Frame):
         font1 = wx.Font(22, wx.MODERN, wx.NORMAL, wx.BOLD)
         font2 = wx.Font(18, wx.MODERN, wx.NORMAL, wx.BOLD)
         font3 = wx.Font(14, wx.TELETYPE, wx.NORMAL, wx.BOLD)
-        
+
         # WX Widget Buttons
         self.dk1Button = wx.Button(panel, label='DK1',
                                     size=wx.Size(50, 100))
@@ -216,15 +222,15 @@ class HansDrumFrame(wx.Frame):
         self.dk6Button = wx.Button(panel, label='DK6',
                                     size=wx.Size(50, 100))
         self.dk1Button.SetFont(font1)
-        self.dk2Button.SetFont(font1)		
-        self.dk3Button.SetFont(font1)		
-        self.dk4Button.SetFont(font1)		
-        self.dk5Button.SetFont(font1)		
+        self.dk2Button.SetFont(font1)
+        self.dk3Button.SetFont(font1)
+        self.dk4Button.SetFont(font1)
+        self.dk5Button.SetFont(font1)
         self.dk6Button.SetFont(font1)
         self.Bind(wx.EVT_BUTTON, self.buttonDKChange)
-        
-        # WX Widget ToggleButton		
-        self.voltb = wx.ToggleButton(panel, -1, 
+
+        # WX Widget ToggleButton
+        self.voltb = wx.ToggleButton(panel, -1,
         	 label='Volume',
         	 name='Volume',
         	 size=(50,100))
@@ -232,19 +238,19 @@ class HansDrumFrame(wx.Frame):
         	 label='Speed',
         	 name='Speed',
         	 size=(50,100))
-        self.distb = wx.ToggleButton(panel, -1, 
+        self.distb = wx.ToggleButton(panel, -1,
         	 label='Distor\ntion',
         	 name='Distortion',
         	 size=(50,100))
-        self.fretb = wx.ToggleButton(panel, -1, 
+        self.fretb = wx.ToggleButton(panel, -1,
         	 label='Freq\nShift',
         	 name='FreqShift',
         	 size=(50,100))
-        self.chotb = wx.ToggleButton(panel, -1, 
+        self.chotb = wx.ToggleButton(panel, -1,
         	 label='Chorus',
         	 name='Chorus',
         	 size=(50,100))
-        self.revtb = wx.ToggleButton(panel, -1, 
+        self.revtb = wx.ToggleButton(panel, -1,
         	 label='Reverb',
         	 name='Reverb',
         	 size=(50,100))
@@ -255,8 +261,8 @@ class HansDrumFrame(wx.Frame):
         self.chotb.SetFont(font2)
         self.revtb.SetFont(font2)
         self.Bind(wx.EVT_TOGGLEBUTTON, self.tbuttonUpdate)
-        
-        # WX Widget Sliders		 
+
+        # WX Widget Sliders
         self.volslide = wx.Slider(panel, -1, 100.0, 0.0, 200.0,
                                   size=(80, -1),
                                   name=('Volume-param'),
@@ -287,38 +293,38 @@ class HansDrumFrame(wx.Frame):
         self.freslide.SetFont(font2)
         self.choslide.SetFont(font2)
         self.revslide.SetFont(font2)
-		
+
         volcol = wx.BoxSizer(wx.HORIZONTAL)
         volcol.AddStretchSpacer(20)
         volcol.Add(self.volslide, flag=wx.EXPAND|wx.CENTER)
         volcol.SetMinSize(wx.Size(80, -1))
-        volcol.AddStretchSpacer(20)		
+        volcol.AddStretchSpacer(20)
         specol = wx.BoxSizer(wx.HORIZONTAL)
         specol.AddStretchSpacer(20)
         specol.Add(self.speslide, flag=wx.EXPAND|wx.CENTER)
         specol.SetMinSize(wx.Size(80, -1))
-        specol.AddStretchSpacer(20)		
+        specol.AddStretchSpacer(20)
         discol = wx.BoxSizer(wx.HORIZONTAL)
         discol.AddStretchSpacer(20)
         discol.Add(self.disslide, flag=wx.EXPAND|wx.CENTER)
         discol.SetMinSize(wx.Size(80, -1))
-        discol.AddStretchSpacer(20)		
+        discol.AddStretchSpacer(20)
         frecol = wx.BoxSizer(wx.HORIZONTAL)
         frecol.AddStretchSpacer(20)
         frecol.Add(self.freslide, flag=wx.EXPAND|wx.CENTER)
         frecol.SetMinSize(wx.Size(80, -1))
-        frecol.AddStretchSpacer(20)		
+        frecol.AddStretchSpacer(20)
         chocol = wx.BoxSizer(wx.HORIZONTAL)
         chocol.AddStretchSpacer(20)
         chocol.Add(self.choslide, flag=wx.EXPAND|wx.CENTER)
         chocol.SetMinSize(wx.Size(80, -1))
-        chocol.AddStretchSpacer(20)		
+        chocol.AddStretchSpacer(20)
         revcol = wx.BoxSizer(wx.HORIZONTAL)
         revcol.AddStretchSpacer(20)
         revcol.Add(self.revslide, flag=wx.EXPAND|wx.CENTER)
         revcol.SetMinSize(wx.Size(80, -1))
         revcol.AddStretchSpacer(20)
-		
+
         self.Bind(wx.EVT_SLIDER, self.sliderUpdate)
 
         self.timer = wx.Timer(panel, 1)
@@ -332,7 +338,7 @@ class HansDrumFrame(wx.Frame):
         timebox.Add(self.ctime, flag=wx.EXPAND|wx.ALIGN_RIGHT)
         timebox.SetMinSize(wx.Size(20, -1))
         timebox.AddStretchSpacer(2)
-        
+
         panelGrid = wx.FlexGridSizer(rows=4, cols=6, hgap=5, vgap=5)
         # WX Widget Button Row
         panelGrid.Add(self.dk1Button, 0, flag=wx.ALIGN_CENTER|wx.EXPAND)
@@ -342,13 +348,13 @@ class HansDrumFrame(wx.Frame):
         panelGrid.Add(self.dk5Button, 0, flag=wx.ALIGN_CENTER|wx.EXPAND)
         panelGrid.Add(self.dk6Button, 0, flag=wx.ALIGN_CENTER|wx.EXPAND)
         # WX Widget ToggleButton Row
-        panelGrid.Add(self.voltb, 0, flag=wx.ALIGN_CENTER|wx.EXPAND)       
+        panelGrid.Add(self.voltb, 0, flag=wx.ALIGN_CENTER|wx.EXPAND)
         panelGrid.Add(self.spetb, 0, flag=wx.ALIGN_CENTER|wx.EXPAND)
         panelGrid.Add(self.distb, 0, flag=wx.ALIGN_CENTER|wx.EXPAND)
         panelGrid.Add(self.fretb, 0, flag=wx.ALIGN_CENTER|wx.EXPAND)
         panelGrid.Add(self.chotb, 0, flag=wx.ALIGN_CENTER|wx.EXPAND)
         panelGrid.Add(self.revtb, 0, flag=wx.ALIGN_CENTER|wx.EXPAND)
-        # WX Widget Slider Row		
+        # WX Widget Slider Row
         panelGrid.Add(volcol, 0, flag=wx.ALIGN_CENTER|wx.EXPAND)
         panelGrid.Add(specol, 0, flag=wx.ALIGN_CENTER|wx.EXPAND)
         panelGrid.Add(discol, 0, flag=wx.ALIGN_CENTER|wx.EXPAND)
@@ -364,23 +370,23 @@ class HansDrumFrame(wx.Frame):
         panelGrid.Add(timebox, 0, wx.ALIGN_CENTER|wx.EXPAND)
         for i in range(6):
             panelGrid.AddGrowableCol(i)
-        panelGrid.AddGrowableRow(2)		
+        panelGrid.AddGrowableRow(2)
 
         panel.SetSizer(panelGrid)
         panelGrid.Fit(self)
-    
+
     def tbuttonUpdate(self, event):
         tbutton = event.GetEventObject()
         self.modulator.effectchain[tbutton.GetName()] = tbutton.GetValue()
         #print(tbutton.GetName() + " " + str(tbutton.GetValue()))
-		
+
     def sliderUpdate(self, event):
         slider = event.GetEventObject()
         self.modulator.effectchain[slider.GetName()] = slider.GetValue() / 100.0
         #print(slider.GetName() + " " + str(slider.GetValue()/100.0))
 
     def buttonDKChange(self, event):
-        button = event.GetEventObject()        
+        button = event.GetEventObject()
         self.chooser.set_kit(button.GetLabel())
         #print(button.GetName() + " " + button.GetLabel())
 
@@ -388,7 +394,7 @@ class HansDrumFrame(wx.Frame):
         self.ctime.SetValue(time.strftime('%M:%S'))
         self.ctime.Refresh()
         self.timer.Start(1000)
-		
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -399,19 +405,19 @@ if __name__ == "__main__":
                         type=int)
     parser.add_argument('-s', '--sampleroot',
                         help='Path of samples root folder',
-                        default='./samples/')
+                        default='.')
     args = parser.parse_args()
 
     if int(''.join(map(str, getVersion()))) < 76:
         # RawMidi is supported only since Python-Pyo version 0.7.6
         raise SystemError("Please, update your Python-Pyo install" +
                           "to version 0.7.6 or later.")
-    
+
     if sys.platform.startswith("win"):
         server = Server()
     else:
         server = Server(audio='jack', jackname='HANSDRUM')
-    
+
     if args.midi:
         server.setMidiInputDevice(args.midi)
     else:
@@ -422,7 +428,7 @@ if __name__ == "__main__":
             server.setMidiInputDevice(inid)
     server.boot()
     server.start()
-    
+
     modulator = Modulator()
     chooser = Chooser(modulator, sample_root=args.sampleroot)
     midiproc = MidiProc()
