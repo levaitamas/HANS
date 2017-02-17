81ce748a37fe115512a84e2836a012cccac0462d
Tamas Levai <levait@tmit.bme.hu>
2017-02-17 15:12:25 +0100

    drum-brain: add testing mode switch

--- util/drum-brain/web/drum-brain-web.py
+++ util/drum-brain/web/drum-brain-web.py
@@ -8,15 +8,13 @@ Copyright (C) 2016-     Tamás Lévai    <levait@tmit.bme.hu>
 """
 from flask import Flask, request, render_template
 import argparse
-import random
 import socket
-import cgi
 
 app = Flask(__name__)
 drum_addr = 'localhost'
 drum_port = 9998
 
-# use socket.socket to send data, example:
+# use socket.socket to send data
 def sndStr(msg):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(msg, (drum_addr, drum_port))
@@ -38,5 +36,12 @@ def index():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='HANS DRUM Web')
+    parser.add_argument('-t', '--testing',
+                        help='Turn on testing mode',
+                        action='store_true')
     args = parser.parse_args()
-    app.run(host='0.0.0.0', port=8000)
+
+    if args.testing:
+        app.run(host='0.0.0.0', port=8000)
+    else:
+        app.run(host='0.0.0.0', port=80)
