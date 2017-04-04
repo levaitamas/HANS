d1c71ed1841a1f64dae0eed40942b133fb35e326
Tamas Levai <levait@tmit.bme.hu>
2017-04-04 11:00:28 +0200

    Revamp theremin util
    Enable drawing.
    Refactor user interface and audiogen code.
    Disable unnecessary pyo input channels.

--- util/theremin.py
+++ util/theremin.py
@@ -17,21 +17,19 @@ except ImportError:
     raise SystemError("Python-Pyo not found. Please, install it.")
 import sys
 
+
 class AudioGen():
     def __init__(self):
         self.n = pyo.Noise(1e-24)
         self.osc = pyo.FastSine(mul=0)
-        self.delay = pyo.SmoothDelay(self.osc+self.n, delay=0, maxdelay=2)
-        self.reverb = pyo.STRev(self.delay+self.n, bal=0)
+        self.switch = pyo.Switch(self.osc)
+        self.delay = pyo.SmoothDelay(self.switch+self.n, delay=0, maxdelay=2)
+        self.reverb = pyo.STRev(self.switch+self.n, bal=0)
         self.reverb.out()
 
-    def resetChain(self, delay_time, reverb_bal):
-        self.delay = pyo.SmoothDelay(self.osc+self.n,
-                                     delay=delay_time,
-                                     maxdelay=2)
-        self.reverb = pyo.STRev(self.delay+self.n,
-                                bal=reverb_bal)
-        self.reverb.out()
+    def setOsc(self, osc):
+        self.osc = osc
+        self.switch.setInput(self.osc)
 
     def setFreq(self, freq):
         self.osc.setFreq(freq)
@@ -46,11 +44,95 @@ class AudioGen():
         self.reverb.setBal(balance)
 
 
+class ThereminPad(wx.Window):
+    def __init__(self, parent, size, audiogen):
+        styl = wx.NO_FULL_REPAINT_ON_RESIZE | wx.SUNKEN_BORDER
+        super(ThereminPad,
+              self).__init__(parent,
+                             size=size,
+                             style=styl)
+        self.audiogen = audiogen
+        self.SetBackgroundColour('WHITE')
+        self.lines = []
+        self.previousPosition = (0, 0)
+        self.Bind(wx.EVT_SIZE, self.onSize),
+        self.Bind(wx.EVT_IDLE, self.onIdle),
+        self.Bind(wx.EVT_PAINT, self.onPaint),
+        self.Bind(wx.EVT_MOTION, self.onMotion)
+        self.Bind(wx.EVT_LEFT_DOWN, self.onLeftDown)
+        self.Bind(wx.EVT_LEFT_UP, self.onLeftUp)
+        self.Bind(wx.EVT_RIGHT_DOWN, self.onRightDown)
+        self.initBuffer()
+
+    def initBuffer(self):
+        size = self.GetClientSize()
+        self.buffer = wx.EmptyBitmap(size.width, size.height)
+        dc = wx.BufferedDC(None, self.buffer)
+        dc.SetBackground(wx.Brush(self.GetBackgroundColour()))
+        dc.Clear()
+        self.drawLines(dc, *self.lines)
+        self.reInitBuffer = False
+
+    def onMotion(self, event):
+        if event.Dragging() and event.LeftIsDown():
+            freq = event.GetX() * 1.4
+            amp = max(1.0 - float(event.GetY())/self.GetSize()[1], 0)
+            self.audiogen.setFreq(freq)
+            self.audiogen.setAmp(amp)
+
+            dc = wx.BufferedDC(wx.ClientDC(self), self.buffer)
+            currentPosition = event.GetPositionTuple()
+            lineSegment = self.previousPosition + currentPosition
+            self.drawLines(dc, ('Red', [lineSegment]))
+            self.currentLine.append(lineSegment)
+            self.previousPosition = currentPosition
+
+    def onLeftDown(self, event):
+        self.currentLine = []
+        self.previousPosition = event.GetPositionTuple()
+
+    def onLeftUp(self, event):
+        self.audiogen.setAmp(0)
+        self.lines.append(('Red', self.currentLine))
+        self.currentLine = []
+
+    def onRightDown(self, event):
+        self.audiogen.setAmp(0)
+
+    def onSize(self, event):
+        self.reInitBuffer = True
+
+    def onIdle(self, event):
+        if self.reInitBuffer:
+            self.initBuffer()
+            self.Refresh(False)
+
+    def onPaint(self, event):
+        wx.BufferedPaintDC(self, self.buffer)
+
+    def clearScreen(self, event):
+        self.lines = []
+        self.initBuffer()
+        self.Refresh()
+
+    @staticmethod
+    def drawLines(dc, *lines):
+        dc.BeginDrawing()
+        for colour, lineSegments in lines:
+            pen = wx.Pen(wx.NamedColour(colour), 2, wx.SOLID)
+            dc.SetPen(pen)
+            for lineSegment in lineSegments:
+                dc.DrawLine(*lineSegment)
+        dc.EndDrawing()
+
+
+
 class ThereminUI(wx.Frame):
     def __init__(self, parent=None):
         wx.Frame.__init__(self,
                           parent,
                           size=(850, 630),
+                          style=(wx.SYSTEM_MENU|wx.CAPTION|wx.CLOSE_BOX),
                           title='HANS THEREMIN')
         self.audiogen = AudioGen()
         self.initUI()
@@ -60,12 +142,11 @@ class ThereminUI(wx.Frame):
     def initUI(self):
         panel = wx.Panel(self)
         panelBox = wx.BoxSizer(wx.VERTICAL)
-        self.touchRect = wx.Window(panel,
-                              size=wx.Size(700, 500),
-                              style=wx.SUNKEN_BORDER)
-        self.touchRect.SetBackgroundColour('#FEFEFE')
-        self.touchRect.Bind(wx.EVT_MOTION, self.actionOnThereminPad)
+        self.clearText = wx.StaticText(panel, label="Clear Screen")
+        panelBox.Add(self.clearText, flag=wx.EXPAND|wx.CENTER)
+        self.touchRect = ThereminPad(panel, wx.Size(850, 500), self.audiogen)
         panelBox.Add(self.touchRect, flag=wx.EXPAND|wx.CENTER)
+        self.clearText.Bind(wx.EVT_LEFT_UP, self.touchRect.clearScreen)
         controlBox = wx.BoxSizer(wx.HORIZONTAL)
         slideBox = wx.BoxSizer(wx.VERTICAL)
         self.delaySlide = wx.Slider(panel, -1, 0.0, 0.0, 200.0,
@@ -103,17 +184,6 @@ class ThereminUI(wx.Frame):
         panelBox.Add(controlBox, flag=wx.EXPAND|wx.CENTER)
         panel.SetSizer(panelBox)
 
-    def actionOnThereminPad(self, event):
-        if event.LeftIsDown():
-            freq = event.GetX() * 1.4
-            amp = max(1.0 - float(event.GetY()) / self.touchRect.GetSize()[1],
-                      0)
-            self.audiogen.setFreq(freq)
-            self.audiogen.setAmp(amp)
-        elif event.RightIsDown():
-            self.audiogen.setAmp(self.audiogen.osc.mul/2)
-            self.audiogen.setAmp(0)
-
     def changeEffectParameter(self, event):
         obj = event.GetEventObject()
         if obj.GetName() == "delayslider":
@@ -129,27 +199,24 @@ class ThereminUI(wx.Frame):
         self.rcoscButton.SetValue(0)
         obj = event.GetEventObject()
         obj.SetValue(1)
-        self.audiogen.reverb.stop()
-        delayTime = self.delaySlide.GetValue() / 200.0
-        reverbBal = self.reverbSlide.GetValue() / 100.0
         if obj.GetLabel() == "FastSine":
-            self.audiogen.osc = pyo.FastSine(mul=0)
+            self.audiogen.setOsc(pyo.FastSine(mul=0))
         elif obj.GetLabel() == "SuperSaw":
-            self.audiogen.osc = pyo.SuperSaw(mul=0)
+            self.audiogen.setOsc(pyo.SuperSaw(mul=0))
         elif obj.GetLabel() == "Blit":
-            self.audiogen.osc = pyo.Blit(mul=0)
+            self.audiogen.setOsc(pyo.Blit(mul=0))
         elif obj.GetLabel() == "SuperSaw":
-            self.audiogen.osc = pyo.Phasor(mul=0)
+            self.audiogen.setOsc(pyo.Phasor(mul=0))
         elif obj.GetLabel() == "RCOsc":
-            self.audiogen.osc = pyo.RCOsc(mul=0)
-        self.audiogen.resetChain(delayTime, reverbBal)
+            self.audiogen.setOsc(pyo.RCOsc(mul=0))
 
 
 if __name__ == "__main__":
     if sys.platform.startswith("win"):
         server = pyo.Server()
     else:
-        server = pyo.Server(audio='jack', jackname='HANS THEREMIN', duplex=0)
+        server = pyo.Server(audio='jack', jackname='HANS THEREMIN',
+                            duplex=0, ichnls=0)
     server.boot()
     server.start()
     app = wx.App()
