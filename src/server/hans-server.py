7cdd08f1156672ae32a2135059f7cbf7e0ad21f3
Tamas Levai <levait@tmit.bme.hu>
2017-04-14 11:59:07 +0200

    Update copyright boilerplates

--- src/server/hans-server.py
+++ src/server/hans-server.py
@@ -5,6 +5,19 @@ HANS SERVER
 
 Copyright (C) 2015-     Tamás Lévai    <levait@tmit.bme.hu>
 Copyright (C) 2015-     Richárd Beregi <richard.beregi@sztaki.mta.hu>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 try:
     import pyo
--- src/web/hans_web.py
+++ src/web/hans_web.py
@@ -5,7 +5,21 @@ HANS WEB
 
 Copyright (C) 2016-     Richárd Beregi <richard.beregi@sztaki.mta.hu>
 Copyright (C) 2016-     Tamás Lévai    <levait@tmit.bme.hu>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+
 from flask import Flask, render_template, redirect, url_for, request
 import argparse
 import logging
